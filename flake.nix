d5c67e44126712daad60ac533ed37f180989cca2
Andrew Pan <known@unown.me>
2021-05-14 11:36:20 -0500

    flake: simple systemd socket activation module
    - fishcgi.socket on sockets.target
    - fishcgi.service triggered by fishcgi.target
    TODO: fix the probable perms issues and provide configurable user

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       lib = nixpkgs.lib;
       sys = "x86_64-linux";
@@ -19,11 +19,65 @@
       defaultPackage.${sys} = pkgs.stdenv.mkDerivation {
         pname = "fish-cgi";
         version = "HEAD";
-        src = ./.;
+        src = pkgs.nix-gitignore.gitignoreSource [
+          "flake.nix"
+        ] ./.;
         nativeBuildInputs = [ pkgs.bash pkgs.cmake ];
         buildInputs = [ fish pkgs.fcgi ] ++ fish.deps;
 
         cmakeFlags = [ "-DFETCH_FISH=OFF" "-DFISH_PATH=${fish}" ];
+        postInstall = "mkdir -p $out/share; cp $src/examples/index.fish $out/share";
+      };
+
+      devShell.${sys} = self.defaultPackage.${sys}.overrideAttrs (o: {
+        nativeBuildInputs = o.nativeBuildInputs ++ [ pkgs.clang-tools ];
+      });
+
+      # https://github.com/NixOS/nixpkgs/blob/nixos-20.09/nixos/modules/services/web-servers/phpfpm/default.nix
+      # TODO(tny): parameterize
+      nixosModule = { config, lib, ... }: with lib; let cfg = config.services.fishcgi; in {
+        options = {
+          services.fishcgi.enable = mkOption {
+            type = types.bool;
+            default = false;
+          };
+          services.fishcgi.package = mkOption {
+            type = types.package;
+            default = self.defaultPackage.${sys};
+          };
+          services.fishcgi.example = mkOption {
+            type = types.str;
+            default = "${self.defaultPackage.${sys}}/share/index.fish";
+            readOnly = true;
+          };
+
+          services.fishcgi.socket = mkOption {
+            type = types.str;
+            default = "/run/fishcgi.sock";
+            readOnly = true;
+          };
+        };
+        config = lib.mkIf cfg.enable {
+          users.users.fishcgi.isSystemUser = true;
+          systemd.sockets.fishcgi = {
+            wantedBy = [ "sockets.target" ];
+            socketConfig = {
+              SocketUser = "nginx";
+              ListenStream = cfg.socket;
+              Accept = false;
+            };
+          };
+          systemd.services.fishcgi = {
+            after = [ "network.target" ];
+            serviceConfig = {
+              Type = "simple";
+              User = "fishcgi";
+              Group = "nginx";
+              ExecStart = "${cfg.package}/bin/fish-cgi";
+              StandardInput = "socket";
+            };
+          };
+        };
       };
     };
 }
